8838d741df69609b3db1edfee255ca507382af22
Daniel Nuno <daniel.nuno@gmail.com>
2016-06-15 10:03:49 -0500

    Getter Method

--- Date1/src/date1/Date.java
+++ Date1/src/date1/Date.java
@@ -13,6 +13,18 @@ public class Date {
  private int day;
  private int month;
  private int year;
+
+    public int getDay() {
+        return day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getYear() {
+        return year;
+    }
  
 public boolean isLeapYear(int year) {
     return (((year % 4) == 0) && ((year % 100) != 0)) || ((year % 400) == 0);
